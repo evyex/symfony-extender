c32b8917f44da7c9937904f0cd974c1df8115c0e
evgenij-vy <evgenijvy@gmail.com>
2026-01-20 12:54:51 +0200

    Fixed IsGranted compatibility with MapRequestPayload and MapQueryString

---
+++ src/Security/IsGrantedAttributeListenerDecorator.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Evyex\SymfonyExtender\Security;
+
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Http\EventListener\IsGrantedAttributeListener;
+
+#[AsDecorator(IsGrantedAttributeListener::class)]
+final class IsGrantedAttributeListenerDecorator extends IsGrantedAttributeListener
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::CONTROLLER_ARGUMENTS => ['onKernelControllerArguments', -5]];
+    }
+}
