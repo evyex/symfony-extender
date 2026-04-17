2d51d34477fceb60ecfdd499d0eb7b323649ab20
Evgen Yurchenko <evgenijvy@gmail.com>
2026-04-17 15:12:32 +0300

    Remove unused `NodeDefinition` import from `SymfonyExtenderBundle`

--- src/SymfonyExtenderBundle.php
+++ src/SymfonyExtenderBundle.php
@@ -9,7 +9,6 @@ use Evyex\SymfonyExtender\Validator\PhoneNumberValidator;
 use Evyex\SymfonyExtender\ValueResolver\MapEntityCollection\EntityCollectionValueResolver;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
-use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
